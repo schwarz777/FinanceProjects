8e87a10320661801b5523e700a75ab15fd6af7b9
schwarz777 <michaelschwarz777@gmail.com>
2020-06-02 23:09:29 +0200

    -minor improvements -adj method get_tickers_history to allow for normalization

--- MyFuncGeneral.py
+++ MyFuncGeneral.py
@@ -4,6 +4,8 @@ Created on Sun Jun 16 13:23:53 2019
 
 @author: MichaelSchwarz
 """
+
+
 # packages
 # pip install yahoo-finance
 # pip install yfinance
@@ -59,29 +61,50 @@ def cnx_mysqldb(database='fuyu'):
 # results = [r.fetchall() for r in cursor.stored_results()]
 # dfres = pd.DataFrame(results[0], columns = ['parent','child','members'])
 
-#get prices
-def get_tickers_history(start, end, tick_list, this_price):
-    """ arg: `this_price` can take str Open, High, Low, Close, Volume"""
+# get prices
+def get_tickers_history(start, end, tick_list, this_price, normalize_by=False):
+    """ arg: `this_price` can take str Open, High, Low, Close, Volume
+        arg: 'normalize_by' can take :
+                    str 'shortest' to normalize all series by time series that has the latest new price
+                    str 'first_price' to normalize each series by its first price
+                    False for not normalizing
+    """
     import pandas as pd
     import pandas_datareader.data as pdr
-    #make an empty dataframe in which we will append columns
+    # make an empty dataframe in which we will append columns
     dat = pd.DataFrame([])
-    # loop here.
+    first_datapoint = list()
+    # loop here to fetch all price data
     for idx, i in enumerate(tick_list):
-        total = pdr.DataReader(i, 'yahoo', start, end)
-        dat[i] = total[this_price]
+        fetch_new = pdr.DataReader(i, 'yahoo', start, end)
+        new = fetch_new[this_price]
+        new.name = i
+        first_datapoint.append(new.first_valid_index())
+        dat = dat.merge(new, how='outer', left_index=True, right_index=True)
+    if normalize_by == 'shortest':
+        # time series are normalized with the oldest price tick of the shortest  series
+        dat = dat / dat.loc[max(first_datapoint)]
+        print("All time series were normalized by: ", max(first_datapoint))
+    elif normalize_by == 'first_price':
+        # time series are normalized with the oldest price tick of each series
+        dat = dat / dat.min()
+    elif not normalize_by:
+        print("the effective prices are returned")
+    else:
+        print("Your input for normalize_by" '', normalize_by, "' is not defined")
+        exit(0)
     return dat
+    # example
+    # get_tickers_history(start="2015-01-05",end="2020-01-05",tick_list= ['ABBN.SW', 'PRX.AS', 'NSI.AS','KGX.DE','MSFT'],this_price='Close',normalize_by = 'first_price').plot()
 
-#example
+
+# example
 # import datetime as dt
 # tickers = ['ABBN.SW', 'PRX.AS', 'NSI.AS','KGX.DE','MSFT'] # add as many tickers
 # start = dt.datetime(2017, 3,31)
 # end = dt.datetime.today()
 # tickhist = get_tickers_history(start, end, tickers, 'Close')
 # tickhist.plot()
-
-
-
 
 def col(df):
     return list(df.columns.values)
@@ -91,11 +114,6 @@ def get_source(func):
     import inspect
     lines = inspect.getsource(func)
     print(lines)
-
-
-
-
-
 
 # STUFF###########
 ####alternative zu pandas....###
