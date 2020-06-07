ffb0ad00c719436b705a024a0b862d8017c0f7ad
schwarz777 <michaelschwarz777@gmail.com>
2020-06-07 23:58:30 +0200

    -make cluster split interactive with dropdown and callback

--- DashGUI/DashWithTabs.py
+++ DashGUI/DashWithTabs.py
@@ -1,29 +1,37 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
 import sys
-
-# from StockSelection.CompanyClass import Company
-
 sys.path.append(r'C:\Users\MichaelSchwarz\PycharmProjects\FinanceProjects')
 import MyFuncGeneral as my
 
-sys.path.append(r'C:/Users/MichaelSchwarz/PycharmProjects/FinanceProjects/PortfolioConstruction')
-import evaluate_portfolio_exposure as epe
-
-from dash.dependencies import Input, Output
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-# get company info
+# PREPARE TAB1 STOCKS RELATIVE
 sys.path.append(r'C:/Users/MichaelSchwarz/PycharmProjects/FinanceProjects/StockSelection')
 import CompanyClass as c
+
 c1 = c.Company("PRX.AS")
 c2 = c.Company("EVD.DE")
 fig_prep = c1.compare(start="2020-01-05", end="2020-05-25")
 
-# Company.compare(c1, start="2015-01-05", end="2020-05-25")
+# PREPARE TAB2 PORTFOLIO
+sys.path.append(r'C:/Users/MichaelSchwarz/PycharmProjects/FinanceProjects/PortfolioConstruction')
+import evaluate_portfolio_exposure as epe
 
+# dropdown menu
+# get the different Clusternames as Options for the Dropdown Choice for SunburstCluster Center
+cnx = my.cnx_mysqldb('fuyu')
+query = "select distinct NameMotherCluster from ( " + \
+        "select level1 as NameMotherCluster from exposure_clusters_by_level " + \
+        "union select '__________LEVEL2_________' union select distinct level2 from exposure_clusters_by_level )s " + \
+        "where NameMotherCluster is not null"
+dropdownLabClusters = pd.read_sql(query, con=cnx)
+optionsmenu_own = [dict(label=i, value=i) for i in dropdownLabClusters["NameMotherCluster"]]
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# APP START
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
@@ -40,15 +48,41 @@ app.layout = html.Div([
 def render_content(tab):
     if tab == 'tab-1':
         return html.Div([
-            html.H3('Stocks Relative'),
+            html.H3('Performance over time'),
             dcc.Graph(id='stocks_rel', figure=fig_prep)
         ])
+
     elif tab == 'tab-2':
         return html.Div([
-            html.H3('Relative Development over time'),
-            dcc.Graph(id='portfolio_view', figure=epe.evaluate_clusters_in_sunburst(most_inner_cluster='GICS'))
+            html.H3('Composition by different Clusters'),
+            dcc.Dropdown(
+                id='dropdown-id',
+                options=optionsmenu_own,
+                value='GICS'
+            ),
+            # dcc.Textarea(id='text', value="test"),
+            dcc.Graph(id='graph-portfolio-view')
         ])
 
 
+@app.callback(
+    dash.dependencies.Output('graph-portfolio-view', 'figure'),
+    [dash.dependencies.Input('dropdown-id', 'value')]
+)
+def update_output_div(chosen_cluster):
+    clust = epe.evaluate_clusters_in_sunburst(chosen_cluster)
+    f = px.sunburst(clust,
+                    names='child',
+                    parents='parent',
+                    values='members',
+                    )
+    return f
+
+
+# app.css.append_css({
+#     'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+# })
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
+    # app.run_server(debug=True)
