da3e5c30acff93f19f470a3fd0bd05af4dc82569
schwarz777 <michaelschwarz777@gmail.com>
2021-01-06 07:59:03 +0100

    -new function: fill_up_series_with_closest -smaller improvements

--- MyFuncGeneral.py
+++ MyFuncGeneral.py
@@ -27,11 +27,13 @@ def get_last_close(ticker, pricesource='yahoo'):
     try:
         px = pdr.DataReader(ticker, 'yahoo', from_d, to_d)
         pxs = pd.to_numeric(px.loc[:, "Adj Close"])
-        return pxs[len(pxs) - 1]
+        return pxs.last('1D')
     except:
         print("unable loading", ticker, "from ", from_d, "to ", to_d)
 
-    # example: get_last_close('URW.AS')
+    # example:
+    # ticker = 'URW.AS'
+    # get_last_close('URW.AS')
 
 
 # db connection
@@ -42,6 +44,8 @@ def cnx_mysqldb(database='fuyu'):
                                   host='127.0.0.1',
                                   database=database)
     return cnx
+
+
 # df = pd.read_sql(query, con=cnx)
 # import pandas as pd
 # f
@@ -49,7 +53,7 @@ def cnx_mysqldb(database='fuyu'):
 
 def date2mysqldatestring(date):
     import datetime as dt
-    assert(type(date) == dt.datetime)
+    assert (type(date) == dt.datetime)
     return str(date)[0:10]
 
 
@@ -65,7 +69,8 @@ def date2mysqldatestring(date):
 # dfres = pd.DataFrame(results[0], columns = ['parent','child','members'])
 
 # get prices
-def get_tickers_history(start, end, tick_list, this_price, normalize_by=False):
+import datetime as d
+def get_tickers_history(tick_list:list, start, end = d.date.today() , this_price = 'Close', normalize_by=False):
     """ arg: `this_price` can take str Open, High, Low, Close, Volume
         arg: 'normalize_by' can take :
                     str 'shortest' to normalize all series by time series that has the latest new price
@@ -102,6 +107,7 @@ def get_tickers_history(start, end, tick_list, this_price, normalize_by=False):
     return dat
     # example
     # get_tickers_history(start="2017-01-05",end="2020-01-05",tick_list= ['ABBN.SW', 'THBEV.SI', 'NSI.AS','KGX.DE','MSFT'],this_price='Close',normalize_by = 'first_price').plot()
+    #get_tickers_history(tick_list= ['ABBN.SW', 'THBEV.SI', 'NSI.AS','KGX.DE','MSFT'],start="2017-01-05")
 
 
 # example
@@ -109,7 +115,7 @@ def get_tickers_history(start, end, tick_list, this_price, normalize_by=False):
 # tickers = ['ABBN.SW', 'PRX.AS', 'NSI.AS','KGX.DE','MSFT'] # add as many tickers
 # start = dt.datetime(2017, 3,31)
 # end = dt.datetime.today()
-# tickhist = get_tickers_history(start, end, tickers, 'Close')
+# tickhist = get_tickers_history(tickers,start, end, , 'Close')
 # tickhist.plot()
 
 def col(df):
@@ -120,6 +126,7 @@ def get_source(func):
     import inspect
     lines = inspect.getsource(func)
     print(lines)
+
 
 # STUFF###########
 ####alternative zu pandas....###
@@ -142,6 +149,28 @@ def get_source(func):
 def check_numeric(x):
     if not any([isinstance(x, int),
                 isinstance(x, float),
-           #     isinstance(x, long),
+                #     isinstance(x, long),
                 isinstance(x, complex)]):
         raise ValueError('{0} is not numeric'.format(x))
+
+import pandas as pd
+def fill_up_series_with_closest(long_series:pd.Series, short_series:pd.DataFrame, closest_in='backward'):
+    from datetime import datetime, timedelta
+    assert(closest_in in ['backward' ,'forward','nearest'])
+    if closest_in == 'past':
+        long_series.sort_values(ascending = True)
+        short_series.sort_index(ascending = True)
+    else:
+        long_series.sort_values(ascending=False)
+        short_series.sort_index(ascending = False)
+
+    #merge the dates that fit
+    if closest_in == 'backward':
+        short_series.index = short_series.index +  timedelta(days=1) #trick: just go one day forward with to_merge series to make comparison backward_or_equal!
+    else:
+        short_series.index = short_series.index - timedelta(days=1)  # trick: just go one day forward with to_merge series to make comparison backward_or_equal!
+    merged_series = pd.merge_asof(long_series.to_frame(),short_series,left_index=True, right_index=True,direction=closest_in)
+    return merged_series
+    #long_series= pd.Series(range(20),index=pd.date_range('2018-09-01', periods=20, freq='m'),name='long')
+    #short_series =pd.Series(range(1,5),index=pd.date_range('2017-01-01',periods=4,freq='y'),name='short')
+    #fill_up_series_with_closest(long_series,short_series.to_frame())
