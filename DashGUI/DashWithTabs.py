287c22e09f463dbdc48b879695d0cfa466a39520
schwarz777 <michaelschwarz777@gmail.com>
2021-03-14 20:58:22 +0100

    -activate callback for multi-select kfs (spider chart)

--- DashGUI/DashWithTabs.py
+++ DashGUI/DashWithTabs.py
@@ -35,7 +35,8 @@ def create_companies():
                 cobj = c.Company(i)
             except:
                 print("For selected Company "+i+" the Company-Object could not be generated")
-        return  cobj # example dat
+    cnx.close()
+    return  cobj # example dat
     # arb_company=create_companies()
 
 
@@ -45,14 +46,32 @@ datrange = pd.date_range(default_start,default_end)
 numdate = [x for x in range(len(datrange))]
 arb_comp = create_companies()
 
+# DEFINE DROPDOWN MENUS
 # kf selection for rel. valuation
 all_kf = ['AssetBase', 'Operating Earnings',  'NetIncome', 'operating CF',
           'Operating Earnings incl non-core', 'Operating Earnings after capex',
           'Operating Earnings Equ', 'Operating Earnings Equ incl non-core', 'Operating Earnings after capex Equ',
          'operating CF after capex']
-chosen_kfs = all_kf[0:4]
-hist_kf = "NetIncome"
+
 optionsmenu_kf = [dict(label=i, value=i) for i in all_kf]
+chosen_kfs = all_kf[0:4]
+hist_kf = all_kf[0]
+
+# display as
+display_vls = ['YtFV' , 'Premium/discount', 'Multiple','Other']
+optionsmenu_display_as = [dict(label=i, value=i) for i in display_vls]
+
+# cluster
+cnx = my.cnx_mysqldb('fuyu')
+query = "select * from vlc_all_exposure_clusters_with_id where left(CatNameInTree,4) not in ('MSSC','MSRC')"
+available_clusters = pd.read_sql(query, con=cnx)
+cnx.close
+optionsmenu_companies_of_cluster=[]
+for i in range(len(available_clusters)):
+    optionsmenu_companies_of_cluster.append(dict(label=available_clusters.loc[i,"CatNameInTree"],
+                                    value =available_clusters.loc[i,"ID"] )
+                               )
+
 
 
 # PREPARE TAB3 PORTFOLIO
@@ -67,6 +86,7 @@ query = "select distinct NameMotherCluster from ( " + \
         "union select '__________LEVEL2_________' union select distinct level2 from exposure_clusters_by_level )s " + \
         "where NameMotherCluster is not null"
 dropdownLabClusters = pd.read_sql(query, con=cnx)
+cnx.close()
 optionsmenu_cluster = [dict(label=i, value=i) for i in dropdownLabClusters["NameMotherCluster"]]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -97,8 +117,7 @@ def render_content(tab):
             dcc.Graph(id='stocks_rel', figure=fig),
             dcc.RangeSlider(
                     id='my-range-slider',
-                    min= numdate[0],
-                    max=  numdate[-1]
+                      max=  numdate[-1]
                   ,marks =   {0:datrange[0].date() ,
                              int(len(datrange)/6): datrange[int(len(datrange)/6)].date(),
                              int(len(datrange) *2/ 6): datrange[int(len(datrange)*2 / 6)].date(),
@@ -113,19 +132,18 @@ def render_content(tab):
     elif tab == 'tab-2':
         return html.Div([
             dcc.Dropdown(
-                id='cluster-id',
-                options=['Cluster'],
-                value='Cluster'
+                id='clusterdropdown-id',
+                options=optionsmenu_companies_of_cluster,
+                value='none'
             ),
-            dcc.Dropdown(
-                id='ShowValuationAs-id',
-                options=['YtFV'],
-                value='YtFV'
-            )
-            ,
+            # dcc.Dropdown(
+            #     id='ShowValuationAs-id',
+            #     options=optionsmenu_display_as,
+            #     value='YtFV'
+            # ),
             html.Div([
                 html.Div([
-                    html.H6('Historic One Keyfigure'),
+                    html.H6('One Keyfigure Historic'),
                     dcc.Dropdown(
                         id='kfdropdown-id',
                         options=optionsmenu_kf,
@@ -135,14 +153,14 @@ def render_content(tab):
                 ], className="six columns"),
 
                 html.Div([
-                    html.H6('Live various Keyfigures'),
+                    html.H6('Various Keyfigures Live'),
                     dcc.Dropdown(
-                        id='spyderdropdown-id',
+                        id='spiderdropdown-id',
                         options=optionsmenu_kf,
                         value=chosen_kfs,
                         multi=True
                     ),
-                    dcc.Graph(id='graph-valuation-view-spyder')
+                    dcc.Graph(id='graph-valuation-view-spider')
                 ], className="six columns"),
             ], className="row")
 
@@ -170,33 +188,54 @@ def update_output(value):
    fig = arb_comp.compare(start=new_start, end=new_end)
    return fig#'You have selected '+ str(new_start) + 'to' + str(new_end)
 
-#tab2 - hist
+#tab2 - cluster function
+# def update_select_company_by_cluster(clusterdropdown_id):
+#     if any(cluster_id):
+#         query = "insert into issues_yh_now_selected" + \
+#                 "selectTicker_yahoo,c.Issue_name" + \
+#                 "from Issue i inner join" + \
+#                 "vlc_show_exposure_clusters_per_company c on " + \
+#                 "i.issue_Name=c.Issue_name and c.allCat="+str(cluster_id)+ \
+#                 " Limit 20"
+#         cnx = my.cnx_mysqldb('fuyu')
+#         cur = cnx.cursor()
+#         try:
+#             cur.execute(query)
+#             cnx.commit()
+#         except Exception as e:
+#             print("insert selected ticker's members did not work")
+#             print(e)
+#             cnx.rollback()
+#         cnx.close()
+
+#tab2 -
 @app.callback(
     dash.dependencies.Output('graph-valuation-view-hist', 'figure'),
     [dash.dependencies.Input('kfdropdown-id', 'value')]
 )
-def update_val_output_div(chosen_kf):
+#tab2 - hist
+def update_val_output_div(chosen_kf):#,cluster_id):
+   # update_select_company_by_cluster(cluster_id)
     val = arb_comp.get_vals([chosen_kf])[:, 0, :].to_pandas()
     df = val.melt(ignore_index=False)
     fig_histval = px.line(df, x=df.index, y="value", color="company", labels={"value": "YtFV", "variable": "company",
                                                                               'x': "hist reporting period (and price) to calc KF"})
     return fig_histval
 
-#tab2 - spyder
+#tab2 - spider
 @app.callback(
-    dash.dependencies.Output('graph-valuation-view-spyder', 'figure'),
-    [dash.dependencies.Input('spyderdropdown-id', 'value')]
+    dash.dependencies.Output('graph-valuation-view-spider', 'figure'),
+    [dash.dependencies.Input('spiderdropdown-id', 'value')]
 )
 def update_val_output_div(chosen_kf):
-    chosen_kfs = chosen_kf
     if isinstance(chosen_kf, str):
         chosen_kf = [chosen_kf]
-    VAL = arb_comp.get_vals(chosen_kf)
+    VAL = arb_comp.get_vals(chosen_kf,keyinputset='Default', displayMethod='YtFV', WACC=None)
     df = VAL.sel(period=max(VAL._getitem_coord("period"))).to_pandas()
     df = df.melt(ignore_index=False)
-    fig_spyder = px.line_polar(df, r="value", theta=df.index, color="company", line_close=True,
+    fig_spider = px.line_polar(df, r="value", theta=df.index, color="company", line_close=True,
                         color_discrete_sequence=px.colors.sequential.Plasma_r)
-    return fig_spyder
+    return fig_spider
 
 #tab3
 @app.callback(
@@ -218,5 +257,6 @@ app.css.append_css({
 })
 
 if __name__ == '__main__':
+
     app.run_server()
     # app.run_server(debug=True)
